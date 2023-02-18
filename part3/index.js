4c80a33174bc211672751cbe157b0870123ae536
Ajay <exactlyajay.chauhan@gmail.com>
2023-02-18 19:39:17 +0530

    initial work

--- part3/index.js
+++ part3/index.js
@@ -1,41 +1,75 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
 
 let notes = [
   {
-    id:1,
+    id: 1,
     content: "HTML is easy",
     important: true
   },
   {
-    id:2,
-    content: "Browser can execute only Javascript",
-    important:false
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
   },
   {
-    id:3,
-    content:"GET and POST are the most important methods of HTTP protocol ",
-    important:true
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id) 
+  const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
 
-  if(note) {
+  if (note) {
     response.json(note)
-  }else {
+  } else {
     response.status(404).end()
   }
+
+  response.json(note)
 })
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
@@ -46,10 +80,4 @@ app.delete('/api/notes/:id', (request, response) => {
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-})
-
-app.post('/api/notes',(request,response) => {
-  const note = request.body
-  console.log(note);
-  response.json(note)
 })
