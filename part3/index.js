1911278c5141622e54e21f5a10c35487e6629b44
Ajay <exactlyajay.chauhan@gmail.com>
2023-02-20 18:31:35 +0530

    no idea what im doing

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('build'))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let notes = [
   {
@@ -20,6 +35,7 @@ let notes = [
     important: true
   }
 ]
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -76,6 +92,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
