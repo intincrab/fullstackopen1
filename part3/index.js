4c63e005cb464314be848cd5c7536e4d60209269
Ajay <exactlyajay.chauhan@gmail.com>
2023-03-03 09:39:15 +0530

    middleware

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+
+app.use(cors())
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
